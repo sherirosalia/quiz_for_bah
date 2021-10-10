89bbc93ed0396c88c842eda4bf3962ce00d70f20
Sheri Rosalia <sheri.rosalia@gmail.com>
2021-10-10 09:06:19 -0700

    styling and content adjustments

--- app.js
+++ app.js
@@ -94,21 +94,21 @@ function calcscore(){
 
     if(score > 15){
       // alert('you are ready Freddy!')
-      $('#readiness-text').text('\" Congratulations, you are data ready!  Here are some ideas of what you might want to do next to accelerate the monetization of data momentum. \"');
+      $('#readiness-text').text('\" Congratulations, you are data ready!  New opportunities are uncovered by analyzing data to secure your market position and take it to the next level. Some ideas for next steps are to have visualizations to tell the story of your company’s past performance, forecast the future based on the past, automate mundane tasks, or connect supplemental data to get enriched insights for better decision making.  \"');
       $('#ready-link').html('<a href="< https://beyondahunch.com/let-a-bot-do-it/" >monetization of data momentum!</a>');
     }
     else if(score > 11){
 
-      $('#readiness-text').text('\"Well done!  The data experience you have gained so far has powered your success and given you a knowledge base with which to build.  Here is where you might want to go next. \"');
+      $('#readiness-text').text('\"Well done!  The data experience you have gained so far has powered your success and given you a knowledge base with which to build.  Next steps could include architecting a data pipeline, adding improved visualizations, implementing a data management system, or automating repetitive, error-prone activities.  Here’s more on what automation can do for your business.  \"');
       // alert('check out article on selling your data')
       $('#ready-link').html('<a href="< https://beyondahunch.com/let-a-bot-do-it/" >Let a Bot Do It!</a>');
 
     }
     else {
      
-      $('#readiness-text').text('\"Congratulations! You are thinking about the information your business has collected and how to organize it. You have the chance now to architect something that will serve you for many years to come. Lets chat to see how we can support you in your data journey. \"');
+      $('#readiness-text').text('\"Congratulations! You are thinking about the information your business has collected and how to organize it. To organize your data, if it is currently in individual files and spread across your computer or your organization, you will want to compile it all so you can begin assessing it.  Looking at your data, some initial questions to ask will indicate its usability, what we call “clean data”. \"');
      
-      $('#ready-link').html('<a href="< https://beyondahunch.com/let-a-bot-do-it/" >how to organize it!</a>');
+      $('#ready-link').html('<a href="< https://beyondahunch.com/why-is-clean-and-organized-data-important/" >how to organize it!</a>');
 
       // alert('you\'re poised to make a lot of money')
 
